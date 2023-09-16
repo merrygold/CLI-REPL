74bd6d85b5a3f24eefc700d4b7f52ce284d2d190
merrygold <merrygold643@gmail.com>
2023-09-17 02:07:58 +0500

    Backend update

--- server/server.js
+++ server/server.js
@@ -99,6 +99,10 @@ app.get('/', (req, res) => {
   res.send('CLI backend Server is Ready.....');
 });
 
+app.get('/get', (req, res) => {
+  res.send('CLI backend Server is Ready.....');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
